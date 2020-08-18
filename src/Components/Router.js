8c66d383cdd9cb0dbc3e024f779a39a1f5143a97
tongnamuu <thban33@naver.com>
2020-08-19 00:02:06 +0900

    typo correction

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import Home from "Routes/Home";
-import Search from "Routes/Search";
-import TV from "Routes/TV";
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import Home from 'Routes/Home';
+import Search from 'Routes/Search';
+import TV from 'Routes/TV';
 
-export default () => {
+export default () => (
   <Router>
     <>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       <Route path="/search" exact component={Search} />
     </>
-  </Router>;
-};
+  </Router>
+);
