0bf2292236ca1d0ab04b3f00f13df5471382894c
tongnamuu <thban33@naver.com>
2021-04-29 23:51:33 +0900

    Fix Home Structure

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { moviesApi } from '../../api';
+import HomePresenter from './HomePresenter';
+
+export default class extends React.Component {
+  state = {
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
+    error: null,
+    loading: true,
+  };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+
+      const {
+        data: { results: upcoming },
+      } = await moviesApi.upcoming();
+
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
+
+  render() {
+    const { nowPlaying, upcoming, popular, error, loading } = this.state;
+    return (
+      <HomePresenter
+        nowPlaying={nowPlaying}
+        upcoming={upcoming}
+        popular={popular}
+        error={error}
+        loading={loading}
+      />
+    );
+  }
+}
---
+++ src/Routes/Home/HomePresenter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
+  null;
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default HomePresenter;
