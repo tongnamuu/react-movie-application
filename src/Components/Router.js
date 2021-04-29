994b38bdff82045948f4d07e12ed5ba03a588cc9
tongnamuu <thban33@naver.com>
2021-04-29 23:51:11 +0900

    Add components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,6 +8,7 @@ import {
 import Home from 'Routes/Home';
 import Search from 'Routes/Search';
 import TV from 'Routes/TV';
+import Detail from 'Routes/Detail';
 import Header from 'Components/Header';
 
 export default () => (
@@ -17,8 +18,9 @@ export default () => (
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/tv" exact component={TV} />
-        <Route path="/tv/popular" render={() => <h1>Popular</h1>} />
         <Route path="/search" exact component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
---
+++ src/Components/Section.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Section = { title, children }; => ();
+
+Section.PropTypes = {
+    title:PropTypes.string.isRequired,
+    children
+}
