516cb845aaae4d20f64a1a379a7c917de6395cb7
tongnamuu <thban33@naver.com>
2021-04-29 23:51:23 +0900

    Fix Detail structure

---
+++ src/Routes/Detail/DetailContainer.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { moviesApi, tvApi } from '../../api';
+import DetailPresenter from './DetailPresenter';
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      isMovie: pathname.includes('/movies/'),
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const parseId = Number(id);
+    const isMovie = this.state;
+    if (isNaN(parseId)) {
+      return push('/');
+    }
+    let result = null;
+
+    try {
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parseId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parseId));
+      }
+      console.log(result);
+    } catch {
+      this.setState({ error: "Can't find anything" });
+    } finally {
+      this.setState({ loading: false, result });
+    }
+  }
+
+  render() {
+    const { result, error, loading } = this.state;
+    return <DetailPresenter result={result} error={error} loading={loading} />;
+  }
+}
