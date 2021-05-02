b3f30f5faa1027bd66fd603e9c8af79a75ba58fb
tongnamuu <thban33@naver.com>
2021-05-03 01:56:13 +0900

    Fix SearchPresenter with Poster

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from '../../Components/Loader';
+import Section from '../../Components/Section';
+import Message from '../../Components/Message';
+import Poster from '../../Components/Poster';
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
 
 const SearchPresenter = ({
   movieResults,
@@ -8,7 +27,64 @@ const SearchPresenter = ({
   searchTerm,
   loading,
   error,
-}) => null;
+  handleSubmit,
+  updateTerm,
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="Search Movies or TV shows"
+        value={searchTerm}
+        onChange={updateTerm}
+      ></Input>
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="Movie Results">
+            {movieResults.map((movie) => (
+              <Poster
+                key={movie.id}
+                title={movie.original_title}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                isMovie={true}
+                year={movie.release_Date && movie.release_date.substring(0, 4)}
+              ></Poster>
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="TV Results">
+            {tvResults.map((show) => (
+              <Poster
+                key={show.id}
+                title={show.original_name}
+                id={show.id}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                isMovie={false}
+                year={
+                  show.first_air_date && show.first_air_date.substring(0, 4)
+                }
+              ></Poster>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
+    {error && <Message color="#47bf6b" text={error}></Message>}
+    {movieResults &&
+      movieResults.length === 0 &&
+      tvResults &&
+      tvResults.length === 0 && (
+        <Message text={`Nothing Found!!`} color="yellow"></Message>
+      )}
+  </Container>
+);
 
 SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
@@ -17,6 +93,7 @@ SearchPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
   handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
 };
 
 export default SearchPresenter;
