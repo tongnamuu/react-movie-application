ba819607f2daee43151922826606aab435ef997c
tongnamuu <thban33@naver.com>
2021-05-03 01:56:21 +0900

    Fix TVPresenter with Poster

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Section from '../../Components/Section';
 import Loader from '../../Components/Loader';
+import Message from '../../Components/Message';
+import Poster from '../../Components/Poster';
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 0px 20px;
 `;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
@@ -15,19 +17,50 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
-          {topRated.map((show) => show.name)}
+          {topRated.map((show) => (
+            <Poster
+              key={show.id}
+              title={show.original_name}
+              id={show.id}
+              imageUrl={show.poster_path}
+              rating={show.vote_average}
+              isMovie={false}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            ></Poster>
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Shows">
-          {popular.map((show) => show.name)}
+          {popular.map((show) => (
+            <Poster
+              key={show.id}
+              title={show.original_name}
+              id={show.id}
+              imageUrl={show.poster_path}
+              rating={show.vote_average}
+              isMovie={false}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            ></Poster>
+          ))}
         </Section>
       )}
       {airingToday && airingToday.length > 0 && (
         <Section title="Airing Today Shows">
-          {airingToday.map((show) => show.name)}
+          {airingToday.map((show) => (
+            <Poster
+              key={show.id}
+              title={show.original_name}
+              id={show.id}
+              imageUrl={show.poster_path}
+              rating={show.vote_average}
+              isMovie={false}
+              year={show.first_air_date && show.first_air_date.substring(0, 4)}
+            ></Poster>
+          ))}
         </Section>
       )}
+      {error && <Message color="#47bf6b" text={error}></Message>}
     </Container>
   );
 
