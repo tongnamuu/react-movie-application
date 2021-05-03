3013177dc4964520eec8011a79c62df3274b02d1
tongnamuu <thban33@naver.com>
2021-05-03 23:56:21 +0900

    Updates

---
+++ precache-manifest.7d65aedce91444e0254f03288b96afc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad8cecfdaa22c0ab16d3f4314586fd5",
+    "url": "/react-movie-application/index.html"
+  },
+  {
+    "revision": "6ee9a2ff20f2538b091d",
+    "url": "/react-movie-application/static/js/2.00ff257f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movie-application/static/js/2.00ff257f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb63fa14d09ab28cb0d",
+    "url": "/react-movie-application/static/js/main.34f634a2.chunk.js"
+  },
+  {
+    "revision": "30d40b2fd3b976160c21",
+    "url": "/react-movie-application/static/js/runtime-main.8de1b181.js"
+  }
+]);
