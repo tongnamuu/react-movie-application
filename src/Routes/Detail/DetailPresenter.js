da25c7281350b22a2735dd65b88b273323d39b3b
tongnamuu <thban33@naver.com>
2021-05-03 23:52:24 +0900

    Update DetailPresenter.js

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,138 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Loader from '../../Components/Loader';
+import { Helmet } from 'react-helmet';
 
-const DetailPresenter = ({ result, loading, error }) => null;
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  padding: 50px;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
+`;
+
+const Cover = styled.div`
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  width: 30%;
+  height: 100%;
+  border-radius: 5px;
+`;
+
+const Data = styled.div`
+  width: 70%;
+  margin-left: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 32px;
+  margin-bottom: 10px;
+`;
+
+const InfoContainer = styled.div`
+  margin: 20px 0px;
+`;
+
+const Info = styled.span``;
+
+const Divider = styled.span`
+  margin: 0 10px;
+`;
+
+const Overview = styled.p`
+  font-size: 12px;
+  opacity: 0.7;
+  line-height: 1.5;
+  width: 50%;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+  z-index: 0;
+`;
+
+const DetailPresenter = ({ result, loading, error }) =>
+  loading ? (
+    <>
+      <Helmet>
+        <title>Loading</title>
+      </Helmet>
+      <Loader />
+    </>
+  ) : (
+    <Container>
+      <Helmet>
+        <title>
+          {result.original_title ? result.original_title : result.original_name}
+          {''}| tongnamuu
+        </title>
+      </Helmet>
+      <Backdrop
+        bgImage={
+          result.backdrop_path
+            ? `https://image.tmdb.org/t/p/original/${result.backdrop_path}`
+            : require('../../Assets/logo.jpg')
+        }
+      />
+      <Content>
+        <Cover
+          bgImage={
+            result.poster_path
+              ? `https://image.tmdb.org/t/p/original/${result.poster_path}`
+              : require('../../Assets/logo.jpg')
+          }
+        />
+        <Data>
+          <Title>
+            {result.original_title
+              ? result.original_title
+              : result.original_name}
+          </Title>
+          <InfoContainer>
+            <Info>
+              {result.release_date
+                ? result.release_date.substring(0, 4)
+                : result.first_air_date
+                ? result.first_air_date.substring(0, 4)
+                : ''}
+            </Info>
+            <Divider>⨀</Divider>
+            <Info>
+              {result.runtime ? result.runtime : result.episode_run_time[0]} min
+            </Info>
+            <Divider>⨀</Divider>
+            <Info>
+              {result.genres &&
+                result.genres.map((genre, index) =>
+                  index === result.genres.length - 1
+                    ? genre.name
+                    : `${genre.name}/`
+                )}
+            </Info>
+          </InfoContainer>
+          <Overview>{result.overview}</Overview>
+        </Data>
+      </Content>
+    </Container>
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
